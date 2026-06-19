b304443c6ca476367ae80ef77e53910838f34fcc
João Vitor <joao_vqs@hotmail.com>
2026-06-19 18:14:28 -0300

    First commit

---
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/RescueGatitosToWorkApplication.java
@@ -0,0 +1,13 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueGatitosToWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescueGatitosToWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/joaovitorqs/rescue_gatitos_to_work/RescueGatitosToWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueGatitosToWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
