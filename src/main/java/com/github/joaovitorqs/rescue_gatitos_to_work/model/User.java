6ad1ffd30e7501f5d35ba2cf4daa8b6902c0e932
João Vitor <joao_vqs@hotmail.com>
2026-06-22 12:40:56 -0300

    Add User in model

---
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/model/User.java
@@ -0,0 +1,23 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickName;
+    private String email;
+    private String password;
+}
