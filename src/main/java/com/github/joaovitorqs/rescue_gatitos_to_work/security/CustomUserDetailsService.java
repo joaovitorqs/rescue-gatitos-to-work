f75c6b60cfee9efb41af23b176bc5553585e11b2
João Vitor <joao_vqs@hotmail.com>
2026-06-22 20:26:53 -0300

    Add CustomUserDetailsService in security

---
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work.security;
+
+import com.github.joaovitorqs.rescue_gatitos_to_work.model.User;
+import com.github.joaovitorqs.rescue_gatitos_to_work.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
