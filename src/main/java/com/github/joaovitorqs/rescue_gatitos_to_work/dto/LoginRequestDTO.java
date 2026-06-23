ff9c181885d8a9173c4e5e86f372b33ce39ebfc4
João Vitor <joao_vqs@hotmail.com>
2026-06-23 12:11:30 -0300

    Add DTO's in dto

---
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work.dto;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/dto/RegisterResquestDTO.java
@@ -0,0 +1,4 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work.dto;
+
+public record RegisterResquestDTO(String nickName, String email, String password) {
+}
