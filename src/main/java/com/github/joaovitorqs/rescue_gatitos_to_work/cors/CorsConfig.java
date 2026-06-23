3cc986cdb0ef646210b61208664132559c04c89d
João Vitor <joao_vqs@hotmail.com>
2026-06-23 12:04:45 -0300

    Add CorsConfig in cors

---
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/cors/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.github.joaovitorqs.rescue_gatitos_to_work.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") //verificar o endereço do front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+
+    }
+
+}
