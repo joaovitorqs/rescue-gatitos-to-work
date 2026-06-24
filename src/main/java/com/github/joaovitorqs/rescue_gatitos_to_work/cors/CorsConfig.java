89df7ef00d133b694a9d5ca41277728ee0d06ea4
João Vitor <joao_vqs@hotmail.com>
2026-06-24 13:07:30 -0300

    Minor fixes in CorsConfig in cors

--- src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/cors/CorsConfig.java
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4040") //verificar o endereço do front-end
+                .allowedOrigins("http://127.0.0.1:5500/") //verificar o endereço do front-end
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
     }
