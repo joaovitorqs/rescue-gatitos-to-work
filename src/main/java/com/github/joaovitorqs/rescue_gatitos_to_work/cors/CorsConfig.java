ff7f33d89bd4546730f48823c58bb34bfa24ff33
João Vitor <joao_vqs@hotmail.com>
2026-07-21 12:36:54 -0300

    Minor fixes in CorsConfig

--- src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/cors/CorsConfig.java
+++ src/main/java/com/github/joaovitorqs/rescue_gatitos_to_work/cors/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.github.joaovitorqs.rescue_gatitos_to_work.cors;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500/") //verificar o endereço do front-end
+                .allowedOrigins("http://127.0.0.1:5500") //verificar o endereço do front-end
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
     }
-
 }
